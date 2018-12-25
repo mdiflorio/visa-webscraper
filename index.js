1b7fdfcaedfea12d9920d631d73ab62bdeea32d2
Madhava Di Florio <mattdiflorio@gmail.com>
2018-12-25 19:50:29 +0100

    Added scrapping for second page. Removed new lines from data.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var https = require("https");
 var http = require("http");
+const scraper = require("./src/Scraper");
 
 // Max sockets set to 3 to limit error when hitting wikipedia.
 http.globalAgent.maxSockets = 3;
 https.globalAgent.maxSockets = 3;
-const scraper = require("./src/Scraper");
 
 scraper();
