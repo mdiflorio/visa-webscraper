3c93679132253133504e2388e58ed15ae8b0d574
Madhava Di Florio <mattdiflorio@gmail.com>
2018-12-29 23:12:43 +0100

    Clean up DB after updating it.

--- update_db.sql
+++ update_db.sql
@@ -1,6 +1,19 @@
 DELETE FROM restrictions;
+
 load data local infile './output/data.csv' into table restrictions 
            fields terminated by ';' 
            enclosed by '"' 
            lines terminated by '\n' 
            IGNORE 1 LINES;
+
+DELETE FROM restrictions 
+WHERE nationality = 'crew members' 
+OR country = 'eVisa'
+OR visaType = '';
+
+UPDATE restrictions
+SET 
+    nationality = 'Maltese'
+WHERE 
+    nationality = 'holders of passports issued by the Sovereign Military Order of Malta';
+
