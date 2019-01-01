70011f7271e4499abe6d3ab523b945324c0439cb
Madhava Di Florio <mattdiflorio@gmail.com>
2019-01-01 12:48:41 +0100

    Update automation of CSV upload.
    - Removed issue with multiple Malta entries.
    - Added more constraints for removal of useless entries.

--- update_db.sql
+++ update_db.sql
@@ -1,19 +1,21 @@
+-- Wipe DB. 
 DELETE FROM restrictions;
 
+--  Load data from file. 
 load data local infile './output/data.csv' into table restrictions 
            fields terminated by ';' 
            enclosed by '"' 
            lines terminated by '\n' 
            IGNORE 1 LINES;
 
+-- Delete useless entries. 
 DELETE FROM restrictions 
 WHERE nationality = 'crew members' 
 OR country = 'eVisa'
+OR country = 'eVisitor'
+OR visaType = '90 days'
 OR visaType = '';
 
-UPDATE restrictions
-SET 
-    nationality = 'Maltese'
-WHERE 
-    nationality = 'holders of passports issued by the Sovereign Military Order of Malta';
+
+
 
